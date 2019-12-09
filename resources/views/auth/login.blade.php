f525f5c37d7debc16fee1ae3daef3b6060618714
alejandra0370 <noemi.castun@gmail.com>
2019-12-09 12:29:28 -0500

    Add filesAdd files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,10 +9,11 @@
         <div class="row">
             <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
                 <div class="card card-signup">
-                    <form class="form" method="" action="">
+                    <form class="form"  method="POST" action="{{  route('login') }}">
+                            @csrf
                         <div class="header header-primary text-center">
-                            <h4>Sign Up</h4>
-                            <div class="social-line">
+                            <h4>inicio de sesión</h4>
+                 <!--           <div class="social-line">
                                 <a href="#pablo" class="btn btn-simple btn-just-icon">
                                     <i class="fa fa-facebook-square"></i>
                                 </a>
@@ -22,44 +23,40 @@
                                 <a href="#pablo" class="btn btn-simple btn-just-icon">
                                     <i class="fa fa-google-plus"></i>
                                 </a>
-                            </div>
+                            </div> -->
                         </div>
-                        <p class="text-divider">Or Be Classical</p>
+                        <p class="text-divider">Ingresa tus datos</p>
                         <div class="content">
-
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    <i class="material-icons">face</i>
-                                </span>
-                                <input type="text" class="form-control" placeholder="First Name...">
-                            </div>
 
                             <div class="input-group">
                                 <span class="input-group-addon">
                                     <i class="material-icons">email</i>
                                 </span>
-                                <input type="text" class="form-control" placeholder="Email...">
+                                 <input id="email" type="email" placeholder="Email..." class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             </div>
 
                             <div class="input-group">
                                 <span class="input-group-addon">
                                     <i class="material-icons">lock_outline</i>
                                 </span>
-                                <input type="password" placeholder="Password..." class="form-control" />
+                                <input placeholder="Password..." id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" />
                             </div>
 
-                            <!-- If you want to add a checkbox to this form, uncomment this code
+                            <!-- If you want to add a checkbox to this form, uncomment this code-->
 
                             <div class="checkbox">
                                 <label>
-                                    <input type="checkbox" name="optionsCheckboxes" checked>
-                                    Subscribe to newsletter
+                                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    Recordar sesión
                                 </label>
-                            </div> -->
+                            </div> 
                         </div>
                         <div class="footer text-center">
-                            <a href="#pablo" class="btn btn-simple btn-primary btn-lg">Get Started</a>
+                            <button type="submit" class="btn btn-simple btn-primary btn-lg">Ingresar</a>
                         </div>
+                     <!--  <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}-->
+                        </a>
                     </form>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,22 +32,40 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="http://www.creative-tim.com">Creative Tim</a>
+                <a class="navbar-brand" href="{{ url('/') }}">App Shop</a>
             </div>
 
             <div class="collapse navbar-collapse" id="navigation-example">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="../components-documentation.html" target="_blank">
-                            Components
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-                            <i class="material-icons">unarchive</i> Upgrade to PRO
-                        </a>
-                    </li>
-                    <li>
+                      @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
+                                </li>
+                            @endif
+                       @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Desconectarse') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                <!--     <li>
                         <a href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
                             <i class="fa fa-twitter"></i>
                         </a>
@@ -61,16 +79,17 @@
                         <a href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-simple btn-white btn-just-icon">
                             <i class="fa fa-instagram"></i>
                         </a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
         </div>
     </nav>
 
     <div class="wrapper">
-
-    @yield('content')
-</div>
+        
+        @yield('content')
+    
+    </div>
 
 
 </body>
